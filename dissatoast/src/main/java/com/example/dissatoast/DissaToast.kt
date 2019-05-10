0e640fbd114a2e68440f56b4abd45d6180239d92
Janaka Dissanayaka <janaka.d@fidenz.com>
2019-05-10 12:23:46 +0530

    Initial release

---
+++ dissatoast/src/main/java/com/example/dissatoast/DissaToast.kt
@@ -0,0 +1,10 @@
+package com.example.dissatoast
+
+import android.content.Context
+import android.widget.Toast
+
+class DissaToast {
+    fun show(context: Context, message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+}
